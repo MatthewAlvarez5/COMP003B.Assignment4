c0dadc1cb83af064e434d487a68e148d6a263ff2
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-04-17 19:42:39 -0700

    Add project files.

---
+++ COMP003B.Assignment4/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Confirmation(ContactViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Contact", model);
+            }
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ COMP003B.Assignment4/Models/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name {  get ; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Phone { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+    }
+}
